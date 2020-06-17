f0692bcb2ccffce7dd08019322510bc0186bf451
TA QUY <taquy.se@gmail.com>
2020-06-18 00:16:17 +0700

    add config for db

--- src/main/java/fpt/fbiz/fremote/FRemoteApplication.java
+++ src/main/java/fpt/fbiz/fremote/FRemoteApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class DemoApplication {
+public class FRemoteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FRemoteApplication.class, args);
 	}
 
 }
